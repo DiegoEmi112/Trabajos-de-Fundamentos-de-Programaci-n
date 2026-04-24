166099a54519702c936377c05e312ad506b80444
DiegoEmi112 <25041722@itdurango.edu.mx>
2026-04-24 08:35:15 -0600

    Add files via upload

---
+++ 26 de febrero.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+//Programa que imprime el mensaje Hola Mundo
+/*este es un
+comentario de varias Lineas*/
+std::cout<<"Hola 'Mundo' \n"<<std::endl;
+std::cout<<"Hola \"Mundo\"\n"<<std::endl;
+cout<<"Adios\n";
+cout<<"\a";
+cout<<"Imprime un numero: "<<1948*5;
+return 0;
+}
