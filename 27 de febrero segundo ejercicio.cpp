ee27e0965d61d376b82273b797e62a1c9e50bf5c
DiegoEmi112 <25041722@itdurango.edu.mx>
2026-05-05 19:42:51 -0600

    Update 27 de febrero segundo ejercicio.cpp

--- 27 de febrero segundo ejercicio.cpp
+++ 27 de febrero segundo ejercicio.cpp
@@ -1,10 +1,10 @@
+// Nombre Del Alumno: Diego Emiliano Quiñones Barraza 
+// No. De Control: 25041722
 #include <iostream>
 #include<windows.h>//SetConsole
 using namespace std;
 int main(void)
 {
-
-
 SetConsoleOutputCP(CP_UTF8);//p/imprimir chrs especiales
 SetConsoleCP(CP_UTF8);//Leer chrs especiales
 int x;//declara variable entera
