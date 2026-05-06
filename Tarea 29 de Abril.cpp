a32a5bebf5a2e1f5330cd6bfcbd9507444dab96d
DiegoEmi112 <25041722@itdurango.edu.mx>
2026-05-05 18:32:02 -0600

    Add files via upload

---
+++ Tarea 29 de Abril.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 15; // Cantidad de números que queremos mostrar
+    long a = 0, b = 1, siguiente;
+
+    cout << "Serie: ";
+
+    for (int i = 0; i < n; i++) {
+        cout << a << " "; // Imprimimos el número actual
+        
+        siguiente = a + b; // Sumamos los dos anteriores
+        a = b;             // El segundo pasa a ser el primero
+        b = siguiente;     // La suma pasa a ser el segundo
+    }
+
+    return 0;
+}
