30bdbb177337f8d211344708717366b80c8e028e
DiegoEmi112 <25041722@itdurango.edu.mx>
2026-05-28 08:58:46 -0600

    Update 3 de marzo.cpp

--- Unidad_2/3 de marzo.cpp
+++ Unidad_2/3 de marzo.cpp
@@ -1,14 +1,16 @@
- #include<stdio.h>
+// Nombre Del Alumno: Diego Emiliano Quiñones Barraza 
+// No. De Control: 2504172
+#include<stdio.h>
 #include <iostream>
 #include <Windows.h>
 using namespace std;
 int main()
 {
-    SetConsoleOutputCP(CP_UTF8);//Controlar salida
-    SetConsoleCP(CP_UTF8); //Controlar entrada
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8); 
     int x=10,y=3;
-    int z = 10; // Declara variable int con valor 10
-    int *dirz = &z; // Declara puntero (int) y le asigna el contenido de la dirección de z
+    int z = 10; 
+    int *dirz = &z; 
     cout<<x<<"+"<<y<<"="<<x+y<<"\n";//13
     cout<<x<<"-"<<y<<"="<<x-y<<"\n"; //7
     cout<<x<<"*"<<y<<"="<<x*y<<"\n";//30
@@ -21,8 +23,7 @@ int main()
     x>0?cout<<"positivo ": cout<<" negativo ";
     cout<<"\nDame un número entero para saber si es +,- o 0: ";
     cin>>x;
-    x>0?cout<<"positivo ":x<0?cout<<" negativo ": cout<<"es cero";//doble pregunta para considerar el cer
-
+    x>0?cout<<"positivo ":x<0?cout<<" negativo ": cout<<"es cero";
     x=0;
     cout<<"\nx="<<x<<"\n";
     cout<<"x++ = "<<x++<<"\n";
@@ -30,9 +31,8 @@ int main()
 	cout<<"++x = "<<++x;
 	cout<<"\nx="<<x<<"\n";
 	cout<<"y="<<y<<". La dirección de y es "<<&y<<"\n";
-	// Imprimir el valor de y y la dirección almacenada en x
-	cout << "El valor de z: " << z << endl; // Imprime el valor de y
-	cout << "La dirección de z (&z) es: " << &z << endl; // Imprime la dirección de y
+	cout << "El valor de z: " << z << endl;
+	cout << "La dirección de z (&z) es: " << &z << endl; 
 	cout << "La dirección de z almacenada en dirz: " << dirz << endl;
 	cout<<"El contenido en *dirz: "<<*dirz<<endl;
 	return 0;
