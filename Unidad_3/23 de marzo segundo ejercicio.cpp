09343a4c86b11b64269c44760d92abfb1d2775ee
DiegoEmi112 <25041722@itdurango.edu.mx>
2026-05-28 18:42:02 -0600

    Update 23 de marzo segundo ejercicio.cpp

--- Unidad_3/23 de marzo segundo ejercicio.cpp
+++ Unidad_3/23 de marzo segundo ejercicio.cpp
@@ -1,4 +1,5 @@
-/*programa que obtiene el mayor de tres números*/
+// Nombre Del Alumno: Diego Emiliano Quiñones Barraza 
+// No. De Control: 2504172
 #include <stdio.h>
 #include <iostream>
 #include <windows.h>
