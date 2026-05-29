bf4cdcc330f0a015410d3441e20fcecc4cdf91b2
DiegoEmi112 <25041722@itdurango.edu.mx>
2026-05-28 18:48:03 -0600

    Update Tarea 28 de Abril.cpp

--- Unidad_3/Tarea 28 de Abril.cpp
+++ Unidad_3/Tarea 28 de Abril.cpp
@@ -1,6 +1,8 @@
+// Nombre Del Alumno: Diego Emiliano Quiñones Barraza 
+// No. De Control: 2504172
 #include <iostream>
 int main() {
-    int n = 10; // Cantidad de números a generar
+    int n = 10;
     int a = 0, b = 1, siguiente;
     std::cout << "Serie de Fibonacci: ";
     for (int i = 0; i < n; i++) {
